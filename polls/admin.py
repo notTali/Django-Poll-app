1f81e8deabf51dc32c7aa2648291fa720a7803b5
Talifhani Mulaudzi <mtalifhani54@gmail.com>
2022-06-14 19:35:36 +0200

    Display database tables on the admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text',)
+
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ('choice_text',)
+    QuestionAdmin.list_display = ('question_text',)
+
+
+admin.site.register(models.Choice, ChoiceAdmin)
+admin.site.register(models.Question, QuestionAdmin)
